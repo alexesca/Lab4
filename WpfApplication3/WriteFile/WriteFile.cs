37b5f5dca7ced20490fce56ca974a4b105936d52
Andy E. Wold <aewold@ldsbc.edu>
2016-07-13 10:50:40 -0600

    Added a WriteFile.cs class

---
+++ WpfApplication3/WriteFile/WriteFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04
+{
+    public class WriteFile
+    {
+        public static List<Student> writeFile(DataTable table1, String file)
+        {
+            return;
+        }
+    }
+}
