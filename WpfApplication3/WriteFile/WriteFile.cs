37fdf08a7a52383aeb0a2eab6c348d5cffdeb68c
Andy E. Wold <aewold@ldsbc.edu>
2016-07-13 11:01:02 -0600

    Added a WriteFile class from StackOverflow

--- WpfApplication3/WriteFile/WriteFile.cs
+++ WpfApplication3/WriteFile/WriteFile.cs
@@ -10,9 +10,42 @@ namespace Lab_04
 {
     public class WriteFile
     {
-        public static List<Student> writeFile(DataTable table1, String file)
+
+        //SOURCE: http://stackoverflow.com/questions/7090628/datagrid-to-csv-file/7090706 [Andy]
+        public void OnExportGridToCSV(object sender, System.EventArgs e)
         {
-            return;
+            // Create the CSV file to which grid data will be exported.
+            StreamWriter sw = new StreamWriter(Server.MapPath("~/GridData.csv"), false);
+            // First we will write the headers.
+            DataTable dt = ((DataSet)grid1.DataSource).Tables[0];
+
+            int iColCount = dt.Columns.Count;
+            for (int i = 0; i < iColCount; i++)
+            {
+                sw.Write(dt.Columns[i]);
+                if (i < iColCount - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            // Now write all the rows.
+            foreach (DataRow dr in dt.Rows)
+            {
+                for (int i = 0; i < iColCount; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        sw.Write(dr[i].ToString());
+                    }
+                    if (i < iColCount - 1)
+                    {
+                        sw.Write(System.Globalization.CultureInfo.CurrentCulture.TextInfo.ListSeparator);
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
         }
     }
 }
